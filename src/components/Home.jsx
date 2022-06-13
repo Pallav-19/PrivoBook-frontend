9f32f815bc2b660fad78b3b7883630121278ecf2
Pallav <pallavinvizag@gmail.com>
2022-06-13 09:02:17 +0530

    deployment 2.8

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from "react";
 import Notes from "./Notes";
 import Login from "./Login";
+import NoteContext from "./contexts/Notes/noteContext";
 function Home() {
+  const context = useContext(NoteContext);
+  const { getNotes } = context;
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   return (
     <div className="container my-3">
       {localStorage.getItem("token") ? <Notes /> : <Login />}
